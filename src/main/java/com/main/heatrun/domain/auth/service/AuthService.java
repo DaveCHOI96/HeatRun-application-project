b6efc42e4448ae81559943e724c3378df1be82f9
CTK <tesold7467@gmail.com>
2026-05-24 20:38:47 +0900

    accessToken 로그아웃시 블랙리스트로 토큰 탈취 방지

--- src/main/java/com/main/heatrun/domain/auth/service/AuthService.java
+++ src/main/java/com/main/heatrun/domain/auth/service/AuthService.java
@@ -197,6 +197,11 @@ public class AuthService {
 
         // Redis에서 리프레시 토큰 삭제
         jwtProvider.deleteRefreshToken(userId);
+
+        // 엑세스 토큰 로그아웃시 블랙리스트 추가
+        // Redis에서 남아있는 토큰 탈취를 방지 / addBlacklist에서 30분 후 블랙리스트 자동 해제
+        jwtProvider.addBlacklist(userId);
+
         log.info("로그아웃 완료: {}", userId);
     }
 }
